5205ac0f2b643b4e9b3403e26b81f384b0b84fa6
YPivneva <146882428+YPivneva@users.noreply.github.com>
2024-10-30 13:35:43 +0300

    Update tree.js

--- tree.js
+++ tree.js
@@ -1,35 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+
 const tree = (dirPath, depth) => {
-    const fs = require("fs");
-    const path = require("path");
-  
     if (depth < 0) return;
-    else
-      fs.readdir(dirPath, { withFileTypes: true }, (err, files) => {
-        if(err) {
-          console.error(`Error reading the catalog: ${err}`);
-          return;
+
+    fs.readdir(dirPath, { withFileTypes: true }, (err, files) => {
+        if (err) {
+            console.error(`Ошибка чтения каталога: ${err.message}`);
+            return;
         }
-        else{
-            files.forEach((file) => {
-                const filePath = path.join(dirPath, file.name);
-                console.log(file.name);
-        
-                if (file.isDirectory()) {
-                    tree(filePath, depth - 1);
-                }
-            });
-        }   
-      });
-  };
-  
-  function construction() {
+
+        files.forEach(file => {
+            const filePath = path.join(dirPath, file.name);
+            console.log(' '.repeat(depth * 2) + file.name);
+
+            if (file.isDirectory()) {
+                tree(filePath, depth - 1);
+            }
+        });
+    });
+};
+
+const construction = () => {
     const args = process.argv.slice(2);
-    const dirPath = args[0] || ".";
-    const depth = args.includes("--depth")
-      ? parseInt(args[args.indexOf("--depth") + 1])
-      : Infinity;
-  
+    const dirPath = args[0] || '.';
+    const depth = args.includes('--depth') ? parseInt(args[args.indexOf('--depth') + 1]) : Infinity;
+
     tree(dirPath, depth);
-  }
+};
 
 construction();
